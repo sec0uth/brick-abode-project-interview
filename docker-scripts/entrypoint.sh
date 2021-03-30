a12f7f37d8925f63cbc3c35ca1b92eed43a66a57
Developer <sec-authority@protonmail.com>
2021-03-30 17:50:35 -0300

    rm useless pip installation in docker entrypoint

--- docker-scripts/entrypoint.sh
+++ docker-scripts/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-# enforces file existence
-if [ ! -e requirements.txt ]; then
-    echo "[-] missing data source file: requirements.txt"
-    exit 1
-fi
-
 # has key?
 if [ -e ~/.ssh/id_rsa ]; then
     # create ssh agent and export agent environment variables
@@ -15,12 +9,6 @@ if [ -e ~/.ssh/id_rsa ]; then
     # add key to agent
     ssh-add ~/.ssh/id_rsa
 fi
-
-# update pip
-pip3 install -U pip
-
-# make dependencies install
-pip3 install -r requirements.txt
 
 # pass execution to received args in current shell
 "$@"

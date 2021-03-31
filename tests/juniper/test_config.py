4444bf2afac44fe255c3d61fcc464b5ecbb25367
Developer <sec-authority@protonmail.com>
2021-03-31 14:51:44 -0300

    replace specific to a more generic one

--- tests/juniper/test_config.py
+++ tests/juniper/test_config.py
@@ -14,47 +14,74 @@ def test_read_from_file(cfg_template):
     assert type(result) is dict
 
 
-def test_succeed_to_validate_logging(monkeypatch, tmp_path):
+def test_succeed_to_validate_required_fields(monkeypatch, tmp_path):
     """Succeeds to read valid yaml configurations."""
-    # contains variations of console and verbose
-    # characterists, but file is None
-    return_dataset1 = [
-        [True, True, None],
-        [True, False, None],
-        [False, True, None],
-        [False, False, None],
-    ]
+    # create dummy file in temporary dir
+    dummy_file = tmp_path / Path('bar.foo')
+    dummy_file.write_bytes(b'')
 
-    # add combinations with a file as string
-    return_dataset2 =  [returnopts for returnopts in return_dataset1
-                        if returnopts.insert(2, 'foo')]
+    # with `banner` and `config`
+    dataset_v1 = {
+        'ssh': {
+            'host': 'foo',
+        },
+        'changes': {
+            'banner': 'something',
 
-
-    logging_to_ret =  return_dataset1 + return_dataset2
-
-    def load_effect(*_, **__):
-        dataset = logging_to_ret.pop()
-
-        return {
-            'logging': {
-                'console': dataset[0],
-                'verbose': dataset[1],
-                'file': dataset[2],
+            'user': {
+                'name': 'bar',
             },
-        }
 
+            'config': 'any',
+        },
+    }
+
+    # with `config` and `banner_file`
+    dataset_v2 = {
+        'ssh': {
+            'host': 'foo',
+        },
+        'changes': {
+            'banner_file': dummy_file.name,
+
+            'user': {
+                'name': 'bar',
+            },
+
+            'config': 'any',
+        },
+    }
+
+    # with `config_file` and `banner`
+    dataset_v3 = {
+        'ssh': {
+            'host': 'foo',
+        },
+        'changes': {
+            'banner_file': 'something',
+
+            'user': {
+                'name': 'bar',
+            },
+
+            'config': dummy_file.name,
+        },
+    }
+
+    datasets_to_ret = [dataset_v1,
+                       dataset_v2,
+                       dataset_v3,]
+
+    def yaml_load_patch(*_, **__):
+        return datasets_to_ret.pop()
 
     # patch yaml to return expected
-    monkeypatch.setattr(yaml, 'load', load_effect)
-
-    # create dummy file in temporary dir
-    file_path = tmp_path / Path('bar.foo')
-    file_path.write_bytes(b'')
+    monkeypatch.setattr(yaml, 'load', yaml_load_patch)
 
     # loop the size of the datasets and 
     # validate the dataset input
-    for _ in logging_to_ret:
+    for _ in datasets_to_ret:
         try:
-            config.read(file_path)
+            config.read(dummy_file)
         except:
             pytest.fail()

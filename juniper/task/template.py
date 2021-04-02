f3c231b496021a8bada8e3f016e5d8dd46efde74
Developer <sec-authority@protonmail.com>
2021-04-02 16:26:42 -0300

    implement template task

--- juniper/task/template.py
+++ juniper/task/template.py
@@ -1,7 +1,10 @@
 """Load arbitrary configuration with Jinja2 template engine."""
 
 
-from . import abc
+import os
+import tempfile
+
+from . import abc, utils
 
 
 class CfgTemplateTask(abc.AbstractTask):
@@ -9,7 +12,35 @@ class CfgTemplateTask(abc.AbstractTask):
 
     def pre_start(self) -> None:
         """Ensure `config_file` exists when should use it."""
-        pass
+        if self.use_config_file():
+            utils.file_or_fail(self.changes['config_file'])
 
     def run(self) -> None:
         """Start task."""
+        if self.use_config_file():
+            template_path = self.changes['config_file']
+        else:
+            # create a temp file to store text
+            template_path = tempfile.mkstemp()[1]
+
+            # write `config` to file
+            with open(template_path, 'w') as fp:
+                fp.write(self.changes['config'])
+
+        # gather facts
+        facts = self.dev.facts
+
+        self.dev.cu.load(template_path=template_path,
+                         template_vars=facts)
+
+        self.dev.cu.commit()
+
+        if not self.use_config_file():
+            try:
+                os.unlink(template_path)
+            except Exception as exc:
+                print(exc)
+
+    def use_config_file(self) -> bool:
+        """Return whether should use `config_file`."""
+        return not self.changes.get('config')

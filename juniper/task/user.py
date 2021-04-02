a13cbeacdd285e9f42b5e60e59e5d841f10d6784
Developer <sec-authority@protonmail.com>
2021-04-01 22:11:12 -0300

    fix user task password issue

--- juniper/task/user.py
+++ juniper/task/user.py
@@ -7,22 +7,16 @@ from . import abc
 
 
 def secure_hash(password: str) -> str:
-    """
-    Calculate password secure hash of `password`.
-    
-    Use sha256 based hash function to keep compatibility with
-    other operating systems given that `crypt` module relays on
-    OS's routines.
-    """
-    return crypt.crypt(password, crypt.METHOD_SHA256)
+    """Calculate password secure hash of `password`."""
+    return crypt.crypt(password)
 
 
 class UserMgmtTask(abc.AbstractTask):
     """Upsert user on system with provided properties."""
 
     # Juno command to add/edit user
-    command_mask = '''set system login user "{name}" class {class} ''' \
-                    '''authentication encrypted-password "{encrypted_passwd}"'''
+    command_mask = '''set system login user {name} class {class} ''' \
+                    '''authentication encrypted-password {encrypted_passwd}'''
 
     def pre_start(self):
         """Ask for user password when `passwd` config is missing."""
@@ -41,8 +35,8 @@ class UserMgmtTask(abc.AbstractTask):
 
     def run(self):
         """Start task."""
-        # get password
-        passwd = self.changes['user']['passwd']
+        # get password string
+        passwd = str(self.changes['user']['passwd'])
 
         # get pasword hash to send over the wire
         passwd_hash = secure_hash(passwd)
